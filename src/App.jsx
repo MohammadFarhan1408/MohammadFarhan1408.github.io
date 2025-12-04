8a0964c1d9595fe0178fe89c686844ed7050311e
Mohammad Farhan <122987956+MohammadFarhan1408@users.noreply.github.com>
2025-12-04 14:16:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col bg-[#E5E5E5]">
       <main className="grow container mx-auto px-4 py-10">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <p>Hello World!</p>
       </main>
     </div>
   );
